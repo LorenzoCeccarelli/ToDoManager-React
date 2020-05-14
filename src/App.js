a4bc6415abf16e5841188152ad6e8b6edf5385f7
Lorenzo Ceccarelli <lorenzo.ceccarelli97@gmail.com>
2020-05-14 11:24:02 +0200

    add project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Task from "./task.js";
+import Navbar from "./Navbar.js";
+import MainContent from "./MainContent.js";
+import OptionalModal from "./OptionalModal.js";
+import * as moment from "moment"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fakeTasks=[
+  new Task(0,"Fare la spesa", 1, 0, "Rifornimento", moment("2020-12-05 22:45"),0),
+  new Task(1,"Pulire la casa", 0, 1, "Casa", moment("2020-05-08 21:09"),1),
+  new Task(2,"Studiare aw1",1,1,"Università",null,0)
+];
+
+const fakeProjects=[
+  "Rifornimento", "Casa","Università"
+]
+
+const filterList=[
+  "All", "Important", "Today", "Next 7 days", "Private", "Shared with..."
+]
+
+class App extends React.Component {
+  render() {
+      return <div className="App">
+          <Navbar/>
+          <MainContent filterList={filterList} tasks={fakeTasks} filter={filterList[0]} projectList={fakeProjects} />
+          <OptionalModal  />
+      </div>
+
+  }
+
 }
 
 export default App;
